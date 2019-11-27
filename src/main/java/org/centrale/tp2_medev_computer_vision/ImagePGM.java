bab72b04ebda0d309481e2a99f78ddd5650ad0d6
bbeaucamp <benjamin.beaucamp@eleves.ec-nantes.fr>
2019-11-27 22:33:07 +0100

    Adds empty ImagePGM class

---
+++ src/main/java/org/centrale/tp2_medev_computer_vision/ImagePGM.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.centrale.tp2_medev_computer_vision;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class ImagePGM {
+    
+    private String path;
+    private int[][] mat;
+
+    public ImagePGM(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int[][] getMat() {
+        return mat;
+    }
+
+    public void setMat(int[][] mat) {
+        this.mat = mat;
+    }
+    
+    
+    
+}
